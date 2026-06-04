b6443fa0c2e53033580f56f3cf9981142129fbec
Stuart Vinson <svinson621@gmail.com>
2026-06-04 15:06:45 -0400

    initial commit, test working

---
+++ src/App.test.jsx
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { expect, test } from "vitest";
+
+test("render app", () => {
+  render(<App />);
+  expect(
+    screen.getByRole("heading", { name: /get started/i }),
+  ).toBeInTheDocument();
+});
